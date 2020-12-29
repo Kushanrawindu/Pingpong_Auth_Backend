93c043e9d948a6a7040440112e7d933161479496
Kushanrawindu <kushanrawindu@gmail.com>
2020-12-29 18:20:13 +0530

    create supervisor controller

---
+++ app/Http/Controllers/SupervisorController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Supervisor;
+
+class SupervisorController extends Controller
+{
+    public function index(){
+        return Supervisor::all();
+    }
+
+    public function show(Supervisor $supervisor){
+        return $supervisor;
+    }
+
+    public function store(Request $request){
+        $supervisor = Supervisor::create($request->all());
+        return response()->json($supervisor, 201);
+    }
+
+    public function update(Request $request, Supervisor $supervisor){
+        $supervisor->update($request->all());
+ 
+        return response()->json($supervisor, 200);
+    }
+
+    public function delete(Supervisor $supervisor){
+        $supervisor->delete();
+ 
+        return response()->json(null, 204);
+    }
+}
